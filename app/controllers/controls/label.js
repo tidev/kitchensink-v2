8feabacd85227c11099809b92d19492a4cbabfd4
hansemannn <hansemannn@users.noreply.github.com>
2016-10-29 21:34:39 +0200

    Add label properties

--- app/controllers/controls/label.js
+++ app/controllers/controls/label.js
@@ -6,3 +6,19 @@
 (function constructor(args) {
     
 })(arguments[0] || {});
+
+function changeToCenterAlignment() {
+    $.myLabel.setTextAlign("center");
+}
+
+function changeToRightAlignment() {
+    $.myLabel.setTextAlign("right");
+}
+
+function changeToJustifyAlignment() {
+    $.myLabel.setTextAlign(3); // or "justify" in Titanium 6.1.0 and later (TIMOB-3408)
+}
+
+function changeColor() {
+    $.myLabel.setColor("red"); // or: "#ff0", "#ff0000", rgba('255, 0,0 , 1.0')
+}
