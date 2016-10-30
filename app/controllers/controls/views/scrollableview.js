7aaedc9dbd71b90b200328fb828cd8e9f530e7d8
hansemannn <hansemannn@users.noreply.github.com>
2016-10-30 11:22:11 +0100

    Add scrollable view example

--- app/controllers/controls/views/scrollableview.js
+++ app/controllers/controls/views/scrollableview.js
@@ -1,3 +1,4 @@
+var log = require("log");
 
 /**
  * The scoped constructor of the controller.
@@ -6,3 +7,34 @@
 (function constructor(args) {
     
 })(arguments[0] || {});
+
+function scrollToView() {
+    $.scrollable.scrollToView(1); // Index or view
+}
+
+function addNewView() {
+    var newView = Ti.UI.createView({
+        backgroundColor: 'rgba(' + _.random(0,255) + ',' + _.random(0,255) + ',' + _.random(0,255) + ', 1.0)' // Generate rgba-color
+    });
+        
+    $.scrollable.addView(newView);
+    log.args('Ti.UI.ScrollableView added new view at index ' + ($.scrollable.views.length - 1));
+
+    validateButtons();
+}
+
+function removeLastView() {
+    $.scrollable.removeView($.scrollable.views[$.scrollable.views.length - 1]);
+    log.args('Ti.UI.ScrollableView deleted last view');
+    
+    validateButtons();
+}
+
+function scrollableViewDidScroll(e) {
+    log.args('Ti.UI.ScrollableView did scroll to index ' + e.currentPage);
+}
+
+function validateButtons() {
+    $.remove.setEnabled($.scrollable.views.length > 0);
+    $.scrollTo.setEnabled($.scrollable.views.length >= 2);
+}
