364403d3e465dadbb52ca3856990f8fe8354457d
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-11-05 17:19:32 +0100

    Update activity indicator example and add styles

--- app/controllers/controls/activityindicator.js
+++ app/controllers/controls/activityindicator.js
@@ -12,4 +12,15 @@ function showIndicator() {
     $.indicatorDark.show();
     $.indicatorBig.show();
     $.indicatorMessage.show();
+    $.indicatorNavBar.show();
+}
+
+function changeTintColor(e) {
+    var colors = ["red", "green", "blue", "yellow", "purple"];
+
+    $.indicatorLight.setIndicatorColor(colors[_.random(0, colors.length - 1)]);
+    $.indicatorDark.setIndicatorColor(colors[_.random(0, colors.length - 1)]);
+    $.indicatorBig.setIndicatorColor(colors[_.random(0, colors.length - 1)]);
+    $.indicatorMessage.setIndicatorColor(colors[_.random(0, colors.length - 1)]);
+    $.indicatorNavBar.setIndicatorColor(colors[_.random(0, colors.length - 1)]);
 }
