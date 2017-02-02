7d3abf9012841525d5d96ce09a9985e8bdc816e0
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-02-02 22:53:17 +0100

    Update facebook.js

--- app/controllers/mashups/facebook.js
+++ app/controllers/mashups/facebook.js
@@ -5,8 +5,7 @@ var log = require("log");
  * The scoped constructor of the controller.
  **/
 (function constructor() {
-
-    if (OS_ANDROID) { // needed for android
+    if (OS_ANDROID) { // Required for Android
         fb.createActivityWorker({
             lifecycleContainer : $.fbWin
         });
@@ -43,33 +42,33 @@ var log = require("log");
 
     // Set the button state
     function setButtonState() {
-       
+        $.fbLogin.setVisible(!fb.loggedIn);
+        $.fbShare.setVisible(fb.loggedIn);
+        $.fbLogout.setVisible(fb.loggedIn);
+        
         if (fb.loggedIn) {
-            $.fbLogin.setVisible(false);
-            $.fbShare.setVisible(true);
-            $.fbLogout.setVisible(true);
             getGraphPath();
-            log.args("Login", "Logged in");
+            log.args('Login', 'Logged in');
         } else {
-            $.fbLogin.setVisible(true);
-            $.fbLogout.setVisible(false);
-            $.fbShare.setVisible(false);            
-            $.fbUserName.text = "";
-            $.fbFriends.text = "";
+            $.fbUserName.text = '';
+            $.fbFriends.text = '';
             $.fbUserImage.image = null;
-            log.args("Logout", "Logged out");
+            log.args('Logout', 'Logged out');
         }
     }
 
     // gets user profile information (name, picture, friends, posts)
     function getGraphPath() {
-        fb.requestWithGraphPath('me', {'fields':'id,name,picture,friends,posts'}, 'GET', function(e) {
+        fb.requestWithGraphPath('me', {
+            fields: 'id,name,picture,friends,posts'
+        }, 'GET', function(e) {
             if (e.success) {
                 var respObj = JSON.parse(e.result);
                 log.args('Modules.Facebook.requestWithGraphPath', respObj);
+                
                 $.fbUserImage.image = respObj.picture.data.url;
-                $.fbUserName.text = "Welcome " + respObj.name;
-                $.fbFriends.text = "Total Friends " + respObj.friends.summary.total_count;
+                $.fbUserName.text = 'Welcome ' + respObj.name;
+                $.fbFriends.text = 'Total Friends ' + respObj.friends.summary.total_count;
             } else if (e.error) {
                 log.args(e.error);
             } else {
@@ -79,15 +78,14 @@ var log = require("log");
     }
 })();
 
-// logs out the user
+// Logs out the current user
 function logout() {
     fb.logout();
 }
 
-// Logs in the user
+// Logs out the current user
 function login() {
-
-    fb.permissions = ["user_photos", "email", "user_friends", "public_profile", "user_birthday", "user_relationships", "user_likes"];
+    fb.permissions = ['user_photos', 'email', 'user_friends', 'public_profile', 'user_birthday', 'user_relationships', 'user_likes'];
     fb.initialize();
 
     if (!fb.loggedIn) {
@@ -100,8 +98,8 @@ function share() {
 
     fb.presentShareDialog({
         link : 'https://appcelerator.com/',
-        title : 'great product',
-        description : 'Titanium is a great product',
+        title : 'Titanoum rocks!',
+        description : 'Check it out today!',
         picture : 'http://www.appcelerator.com/wp-content/uploads/scale_triangle1.png'
     });
 }
