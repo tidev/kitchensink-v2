2fbd5e21add79f64c99a325759ad49d19c8cd924
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-09-08 17:02:09 +0200

    Finish audio-recorder example

--- app/controllers/phone/audioRecorder.js
+++ app/controllers/phone/audioRecorder.js
@@ -1,55 +1,60 @@
-var recorder = null;
+var log,
+		audioRecorder,
+		record;
+
+/**
+ * The scoped constructor of the controller.
+ **/
+(function constructor(args) {
+		log = require('log');
+		audioRecorder = Ti.Media.createAudioRecorder();
+})(arguments[0] || {});
 
 function onOpen() {
-	if (!Ti.Media.hasAudioPermissions()) {
-		Ti.Media.requestAudioPermissions(function(e) {
-			if (!e.success) {
-				alert('Audio Recording Permissions not granted. Skipping!');
-				return;
-			}		
-			
-			Ti.API.info('Audio Recorder Permissions now granted!');
-			initializeAudioEvents();
-		});
-	} else {
-		Ti.API.info('Audio Recorder Permissions already granted!');
-		initializeAudioEvents();
-	}
+    if (!Ti.Media.hasAudioRecorderPermissions()) {
+        Ti.Media.requestAudioRecorderPermissions(function (e) {
+            if (e.success) {
+							$.startRecordingButton.setVisible(true);
+            } else {
+							Ti.API.error('Error: Unable to request audio recorder permissions:');
+							Ti.API.error(e);
+						}
+        });
+    } else {
+				$.startRecordingButton.setVisible(true);
+    }
 }
- 
-function initializeAudioEvents() {
-	recorder = Ti.Media.createAudioRecorder();
 
-	Ti.App.addEventListener('resumed', onResume);
-	Ti.App.addEventListener('pause', onPause);
+function startRecording() {
+    audioRecorder.start();
+
+		$.startRecordingButton.setVisible(false);
+		$.pauseRecordingButton.setVisible(true);
+		$.stopRecordingButton.setVisible(true);
 }
- 
-function onResume() {
-	if (!recorder) {
-		return;
-	}
- 
-	recorder.stop();
- 
-	Ti.Media.audioSessionCategory = Ti.Media.AUDIO_SESSION_CATEGORY_PLAYBACK;
-	
-	log.args('category: ' + Ti.Media.audioSessionCategory);
-	log.args('playback: ' + Ti.Media.AUDIO_SESSION_CATEGORY_PLAYBACK);
-	
-	log.args('onResume');
- 
+
+function pauseRecording() {
+    if (audioRecorder.getPaused()) {
+        $.pauseRecordingButton.setTitle('Pause');
+        audioRecorder.resume()
+    } else {
+        $.pauseRecordingButton.setTitle('Resume');
+        audioRecorder.pause();
+    }
 }
- 
-function onPause() {
-	if (!recorder) {
-		return;
-	}
- 
-	Ti.Media.audioSessionCategory = Ti.Media.AUDIO_SESSION_CATEGORY_PLAY_AND_RECORD;
- 
-	log.args('category: ' + Ti.Media.audioSessionCategory);
-  log.args('play_and_record: ' + Ti.Media.AUDIO_SESSION_CATEGORY_PLAY_AND_RECORD);
-	log.args('onPause');
-    
-	recorder.start();
+
+function stopRecording() {
+    record = audioRecorder.stop();
+		
+		$.startRecordingButton.setVisible(true);
+		$.playRecordingButton.setVisible(true);
+		$.pauseRecordingButton.setVisible(false);
+		$.stopRecordingButton.setVisible(false);
+}
+
+function playRecording() {
+    var audioPlayer = Ti.Media.createAudioPlayer({
+			url: record.getNativePath()
+		});
+    audioPlayer.start();
 }
