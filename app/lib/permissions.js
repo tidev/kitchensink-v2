cb3d59fd0f76f1870bb96096058ab00156f138db
Michael <m1ga@users.noreply.github.com>
2017-09-14 19:33:29 +0200

    various Android fixes (#12)

---
+++ app/lib/permissions.js
@@ -0,0 +1,34 @@
+exports.checkCameraPermission = function(clb) {
+	var hasCameraPermissions = Ti.Media.hasCameraPermissions();
+
+	if (hasCameraPermissions) {
+		clb();
+		return;
+	}
+
+	if (OS_IOS) {
+		var map = {};
+		map[Ti.Media.CAMERA_AUTHORIZATION_AUTHORIZED] = 'CAMERA_AUTHORIZATION_AUTHORIZED';
+		map[Ti.Media.CAMERA_AUTHORIZATION_DENIED] = 'CAMERA_AUTHORIZATION_DENIED';
+		map[Ti.Media.CAMERA_AUTHORIZATION_RESTRICTED] = 'CAMERA_AUTHORIZATION_RESTRICTED';
+		map[Ti.Media.CAMERA_AUTHORIZATION_NOT_DETERMINED] = 'CAMERA_AUTHORIZATION_NOT_DETERMINED';
+
+		var cameraAuthorizationStatus = Ti.Media.cameraAuthorizationStatus;
+		if (cameraAuthorizationStatus === Ti.Media.CAMERA_AUTHORIZATION_RESTRICTED) {
+			return;
+		} else if (cameraAuthorizationStatus === Ti.Media.CAMERA_AUTHORIZATION_DENIED) {
+			return;
+		}
+	}
+
+	Ti.Media.requestCameraPermissions(function(e) {
+		if (e.success) {
+			clb();
+			return;
+		} else if (OS_ANDROID) {
+			return;
+		} else {
+			return;
+		}
+	});
+}
