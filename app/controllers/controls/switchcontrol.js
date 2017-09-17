b6b6f0aaae447d40b668dc03adbceeab49eca470
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-09-17 18:32:18 +0200

    Resolve studio-warnings

--- app/controllers/controls/switchcontrol.js
+++ app/controllers/controls/switchcontrol.js
@@ -9,5 +9,5 @@ var log = require('log');
 
 function switchChanged(e) {
     $.state.setText('The switch value changed to ' + e.value);
-    log.args('Ti.UI.Switch value changed to ' + e.value)
+    log.args('Ti.UI.Switch value changed to ' + e.value);
 }
