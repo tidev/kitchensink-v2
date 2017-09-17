429bb3f619a5505643ffc1427806783d138e90f9
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-09-17 18:35:36 +0200

    More minor code-style fixes

--- app/alloy.js
+++ app/alloy.js
@@ -8,8 +8,9 @@ Alloy.Globals.setAndroidBackButton = function(_window) {
 		var ABH = require('actionbar').actionBarHelper;
 		var actionBarHelper = new ABH(_window);
 
-		if(_window.title && _window.title.length > 0)
+		if (_window.title && _window.title.length > 0) {
 			actionBarHelper.setTitle(_window.title);
+		}
 
 		actionBarHelper.setUpAction(function() {
 			_window.close();
