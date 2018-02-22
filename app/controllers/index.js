c09853319e5557f6c7789e7f9b4a81480997c05e
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-02-22 12:47:14 +0100

    Fix dialog from popping up again, fix too long scrollable textx

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,7 +13,7 @@ var log = require('log');
 })();
 
 function validateDocsInfo() {
-    if (Ti.App.Properties.getBool('showNotice', true) === true) {
+    if (!Ti.App.Properties.getBool('noticeShown', false)) {
     	var alertNotice = Ti.UI.createAlertDialog({
     		title: 'Notice',
     		message: 'While this KitchenSink provides an extensive demonstration of the Titanium API, it does not include examples for every available component. Please refer to our documentation for more details.',
@@ -26,7 +26,7 @@ function validateDocsInfo() {
     		if (e.index === 1) {
     			Ti.Platform.openURL('https://docs.appcelerator.com/platform/latest/');
     		} else if (e.index === 2) {
-    			Ti.App.Properties.setBool('showNotice', false);
+    			Ti.App.Properties.setBool('noticeShown', true);
     		}
     	});
 
@@ -36,5 +36,4 @@ function validateDocsInfo() {
 
 function handleShortcutItem(e) {
     log.args('Ti.App.iOS.shortcutitemclick', e);
-
 }
