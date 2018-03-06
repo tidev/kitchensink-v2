844f2e563f2d9a00e4eb1e3c01afc91e15756c04
Rene Pot <rene@topener.nl>
2018-03-06 15:38:21 +0100

    fixes based on comments

--- app/controllers/phone/map.js
+++ app/controllers/phone/map.js
@@ -1,12 +1,12 @@
 var log = require('log');
 
 var clickedAnnotation = null;
-function handleMapClick(e){
+function handleMapClick(e) {
     // check if annotation was clicked
-    if (e.hasOwnProperty('annotation')){
+    if (e.hasOwnProperty('annotation')) {
         
         // check if the annotation was selected or deselected based on previous state
-        if (e.annotation.id == clickedAnnotation){
+        if (e.annotation.id == clickedAnnotation) {
             log.args('annotation deselected', e.annotation);
             clickedAnnotation = null;
         } else {
@@ -14,13 +14,13 @@ function handleMapClick(e){
             clickedAnnotation = e.annotation.id;
         }
     } else {
-        if (e.clicksource == 'circle'){
+        if (e.clicksource == 'circle') {
             log.args('circle clicked', e.latitude, e.longitude);
         }
     }
 }
 
-/* right now alloy doesn't add circles/polylines/polygons to map properly when they're created in alloy
+/* Right now Alloy doesn't add circles/polylines/polygons to map properly when they're created in Alloy
  * See https://jira.appcelerator.org/browse/ALOY-1608 for more info
  * For now, either create them in the controller or add them manually
  */
