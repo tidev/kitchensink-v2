2f31da91959929d82d785393bd1b051181a22176
Rene Pot <rene@topener.nl>
2018-03-06 16:45:53 +0100

    fix equals notation

--- app/controllers/phone/map.js
+++ app/controllers/phone/map.js
@@ -6,7 +6,7 @@ function handleMapClick(e) {
     if (e.hasOwnProperty('annotation')) {
         
         // check if the annotation was selected or deselected based on previous state
-        if (e.annotation.id == clickedAnnotation) {
+        if (e.annotation.id === clickedAnnotation) {
             log.args('annotation deselected', e.annotation);
             clickedAnnotation = null;
         } else {
@@ -14,7 +14,7 @@ function handleMapClick(e) {
             clickedAnnotation = e.annotation.id;
         }
     } else {
-        if (e.clicksource == 'circle') {
+        if (e.clicksource === 'circle') {
             log.args('circle clicked', e.latitude, e.longitude);
         }
     }
