eff1279871d7fb25106e48c1bf74db8c88df87cd
Ewan Harris <ewanharris93@gmail.com>
2018-04-16 22:24:58 +0100

    Remove showWindowWithToolbar function

--- app/controllers/controls/toolbar.js
+++ app/controllers/controls/toolbar.js
@@ -11,8 +11,3 @@ function sayHello({ source }) {
 	alert(`Hello from ${source.title}`);
 	logger.log(`Ti.UI.Toolbar selected button with title: ${source.title}`);
 }
-
-function showWindowWithToolbar() {
-	const component = Alloy.createController('controls/toolbarAsActionBar').getView();
-	Alloy.CFG.tabGroup.getActiveTab().open(component);
-}
