4ce512e0a38949ad2355257881c893d1fb71f9b8
Rene Pot <rene@topener.nl>
2018-04-30 10:13:02 +0200

    initial for audioplayer

--- app/controllers/phone/audioPlayer.js
+++ app/controllers/phone/audioPlayer.js
@@ -4,3 +4,20 @@
 (function constructor() {
 	// TODO: Migrate from https://github.com/appcelerator/KitchenSink/blob/stable/Resources/ui/common/phone/audio_player.js
 }());
+
+
+
+
+function handleOpen(){
+  $.player.start();
+}
+
+function handleClose(){
+    if ($.player.playing || $.player.paused) {
+      $.player.stop();
+    }
+}
+
+function audioProgression(e){
+  console.log(e);
+}
