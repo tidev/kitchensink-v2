d70b20a3a81a18ce2e5fdf9717eacccbe102a705
Ewan Harris <ewanharris93@gmail.com>
2019-03-13 10:30:20 +0000

    chore(deps): move away from grunt to npm scripts (#42)
    This moves away from the grunt setup into npm scripts and fixes any errors that came up.

--- app/controllers/phone/audioPlayer.js
+++ app/controllers/phone/audioPlayer.js
@@ -1,90 +1,91 @@
 let closingWindow = false;
 
 if (OS_IOS) {
-  Ti.Media.audioSessionCategory = Ti.Media.AUDIO_SESSION_CATEGORY_PLAYBACK;
+	Ti.Media.audioSessionCategory = Ti.Media.AUDIO_SESSION_CATEGORY_PLAYBACK;
 }
 
 function handleOpen() {
-  audioProgression({
-    progress: 0
-  });
-  $.player.start();
+	audioProgression({
+		progress: 0
+	});
+	$.player.start();
 }
 
 function handleClose() {
-  closingWindow = true;
-  if ($.player.playing || $.player.paused) {
-    $.player.stop();
-  }
-  if (OS_IOS) {
-    Ti.Media.audioSessionCategory = Ti.Media.audioSessionCategory;
-  }
+	closingWindow = true;
+	if ($.player.playing || $.player.paused) {
+		$.player.stop();
+	}
 }
 
 function toggleBattle() {
-  // Ignore button if already playing
-  if ($.battlePlayer.playing) {
-    $.battlePlayer.stop();
-    $.toggleBattle.title = 'Play battle sounds';
-  } else {
-    $.battlePlayer.play();
-    $.toggleBattle.title = 'Stop battle sounds';
-  }
+	// Ignore button if already playing
+	if ($.battlePlayer.playing) {
+		$.battlePlayer.stop();
+		$.toggleBattle.title = 'Play battle sounds';
+	} else {
+		$.battlePlayer.play();
+		$.toggleBattle.title = 'Stop battle sounds';
+	}
 }
 
 function changeVolume(e) {
-  // Both slider & volume is ranged from 0-1
-  $.player.volume = e.value;
+	// Both slider & volume is ranged from 0-1
+	$.player.volume = e.value;
 }
 
 // Android only because STATE_STOPPED doesn't fire there
 function handleMusicComplete() {
-  if (!OS_ANDROID) return;
-  changeMusic({ state: $.player.STATE_STOPPED });
+	if (!OS_ANDROID) {
+		return;
+	}
+	changeMusic({ state: $.player.STATE_STOPPED });
 }
 
-function changeMusic(e){
-  // Restart player when play is stopped
-  if (e.state === $.player.STATE_STOPPED && !closingWindow) {
-    $.player.play();
-  }
+function changeMusic(e) {
+	// Restart player when play is stopped
+	if (e.state === $.player.STATE_STOPPED && !closingWindow) {
+		$.player.play();
+	}
 }
 
 function audioProgression(e) {
-  $.progress.text = timeFormat(e.progress) + '/' + timeFormat($.player.duration);
+	$.progress.text = timeFormat(e.progress) + '/' + timeFormat($.player.duration);
 }
 
 // Android only because STATE_STOPPING/STATE_STOPPED doesn't fire there
 function handleBattleComplete() {
-  if (!OS_ANDROID) return;
-  completeBattle({ state: $.battlePlayer.STATE_STOPPING });
-  completeBattle({ state: $.battlePlayer.STATE_STOPPED });
+	if (!OS_ANDROID) {
+		return;
+	}
+	completeBattle({ state: $.battlePlayer.STATE_STOPPING });
+	completeBattle({ state: $.battlePlayer.STATE_STOPPED });
 }
 
 function completeBattle(e) {
-  if (e.state === $.battlePlayer.STATE_STOPPING) {
-    $.applause.play();
-  } else if (e.state === $.battlePlayer.STATE_STOPPED) {
-    $.toggleBattle.title = 'Play battle sounds';
-  }
+	if (e.state === $.battlePlayer.STATE_STOPPING) {
+		$.applause.play();
+	} else if (e.state === $.battlePlayer.STATE_STOPPED) {
+		$.toggleBattle.title = 'Play battle sounds';
+	}
 }
 
 function timeFormat(time) {
-  time = Math.round(time / 1000);
-  // Hours, minutes and seconds
-  const hrs = ~~(time / 3600);
-  const mins = ~~((time % 3600) / 60);
-  const secs = time % 60;
+	time = Math.round(time / 1000);
+	// Hours, minutes and seconds
+	const hrs = ~~(time / 3600);
+	const mins = ~~((time % 3600) / 60);
+	const secs = time % 60;
 
-  // Output like "1:01" or "4:03:59" or "123:03:59"
-  let result = '';
+	// Output like "1:01" or "4:03:59" or "123:03:59"
+	let result = '';
 
-  if (hrs > 0) {
-    result += String(hrs) + ':' + (mins < 10 ? '0' : '');
-  }
+	if (hrs > 0) {
+		result += String(hrs) + ':' + (mins < 10 ? '0' : '');
+	}
 
-  result += String(mins) + ':' + (secs < 10 ? '0' : '');
-  result += String(secs);
+	result += String(mins) + ':' + (secs < 10 ? '0' : '');
+	result += String(secs);
 
-  return result;
+	return result;
 }
