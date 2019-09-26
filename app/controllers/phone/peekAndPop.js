80b78df89af9e3622cf364da5199aca8e17946eb
Sohail Saddique <ssaddique@axway.com>
2019-09-26 14:56:39 +0100

    feat(ios): add peep/pop support
    - support peek and pop function
    - compatible with 3d or haptic touch

--- app/controllers/phone/peekAndPop.js
+++ app/controllers/phone/peekAndPop.js
@@ -1,6 +1,25 @@
-/**
- * The scoped constructor of the controller.
- **/
-(function constructor() {
+// Assign the 'pop' feature to the button
+$.button.previewContext = $.context;
 
-}());
+function actionTitle(e) {
+    alert("Title: " + e.title + " / Style: " + e.style+" / Index: " + e.index);
+}
+
+function action(e) {
+    alert("Title: " + e.title + " / Style: " + e.style+" / Index: " + e.index);
+}
+
+function subAction(e) {
+    alert("Title: " + e.title + " / Style: " + e.style+" / Subindex: " + e.index);
+};
+
+// Pop the preview
+function pop() {
+    $.detailWin.add($.detailText);
+    $.detailWin.add($.buttonBack);
+    $.detailWin.open();
+}
+
+function goBack() {
+	$.detailWin.close();
+}
