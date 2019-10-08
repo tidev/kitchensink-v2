8be887c6c331e7afb318b18472734e8c51e66e0a
Sohail <ssaddique@axway.com>
2019-10-08 16:37:11 +0100

    feat(android): add identity module (#46)
    * feat(android): add identity module
    
    * chore: optimise code
    
    - Cleaned up code. Removed unnecessary code and optimised for re-usability
    
    * chore: refactor identity.js

--- app/controllers/phone/identity.js
+++ app/controllers/phone/identity.js
@@ -1,7 +1,9 @@
 import Identity from 'ti.identity';
 
+var authPhrase = "";
+
 if (!Identity.isSupported()) {
-  alert('Biometric authentication is not supported on this device!');
+  alert('Biometric authentication is not supported on this device \nor\nno identities are enrolled!');
   $.authenticate.enabled = false;
 }
 
@@ -13,48 +15,44 @@ if (OS_IOS) {
   } else {
     authPhrase = '(None available)';
   }
+  $.authenticate.title = "Authenticate with: " + authPhrase;
 }
 
 if (OS_ANDROID) {
   authPhrase = 'Fingerprint';
+  $.authenticate.title = "Tap here to authenticate";
 }
 
-Identity.AUTHENTICATION_POLICY_PASSCODE;
-
-$.authenticate.title = "Authenticate with: " + authPhrase;
-
-function validate(){
-  Identity.authenticate({
-    reason: 'Please authenticate to access content',
-    fallbackTitle: 'Use passcode instead',
-    cancelTitle: 'Cancel',
-    callback: function(e) {
-        Identity.invalidate();
-        if (!e.success) {
-          alert(e.error);
-          switch(e.code) {
-            case Identity.ERROR_AUTHENTICATION_FAILED: Ti.API.info('Error code is Identity.ERROR_AUTHENTICATION_FAILED'); break;
-            case Identity.ERROR_USER_CANCEL: Ti.API.info('Error code is Identity.ERROR_USER_CANCEL'); break;
-            case Identity.ERROR_USER_FALLBACK: Ti.API.info('Error code is Identity.ERROR_USER_FALLBACK'); break;
-            case Identity.ERROR_SYSTEM_CANCEL: Ti.API.info('Error code is Identity.ERROR_SYSTEM_CANCEL'); break;
-            case Identity.ERROR_PASSCODE_NOT_SET: Ti.API.info('Error code is Identity.ERROR_PASSCODE_NOT_SET'); break;
-            case Identity.ERROR_TOUCH_ID_NOT_AVAILABLE: Ti.API.info('Error code is Identity.ERROR_TOUCH_ID_NOT_AVAILABLE'); break;
-            case Identity.ERROR_TOUCH_ID_NOT_ENROLLED: Ti.API.info('Error code is Identity.ERROR_TOUCH_ID_NOT_ENROLLED'); break;
-            case Identity.ERROR_TOUCH_ID_NOT_ENROLLED: Ti.API.info('Error code is Identity.ERROR_TOUCH_ID_NOT_ENROLLED'); break;
-            case Identity.ERROR_APP_CANCELLED: Ti.API.info('Error code is Identity.ERROR_APP_CANCELLED'); break;
-            case Identity.ERROR_INVALID_CONTEXT: Ti.API.info('Error code is Identity.ERROR_INVALID_CONTEXT'); break;
-            case Identity.ERROR_TOUCH_ID_LOCKOUT: Ti.API.info('Error code is Identity.ERROR_TOUCH_ID_LOCKOUT'); break;
-            default: Ti.API.info('Error code is unknown'); break;
-          }
-        } else {
-          if (Identity.biometryType == Identity.BIOMETRY_TYPE_FACE_ID) {
-            setTimeout(function() {
-              alert('Successfully authenticated!');
-            }, 1000);
-          } else {
-            alert('Successfully authenticated!');
-          }
+function validate() {
+if (OS_ANDROID) {
+  let reason = "Confirm fingerprint to authenticate";
+  let title = "Identity";
+  $.androidFingerprint.setReason(reason);
+  $.androidFingerprint.setTitle(title);
+}
+Identity.authenticate({
+  reason: "Please authenticate to continue",
+  fallbackTitle: "",
+  callback: function(e) {
+    if (OS_IOS) {
+      Identity.invalidate();
+      if (!e.success) {
+        alert(e.error);
+      } else {
+        setTimeout(function() {
+        alert('Successfully authenticated!');
+        }, 1000);
+      }
+    } else if (OS_ANDROID) {
+      if(e.success) {
+        $.androidFingerprint.success();
+      } else {
+        $.androidFingerprint.failure(e.error);
       }
     }
   }
-)};
+});
+if (OS_ANDROID) {
+  $.androidFingerprint.show(function() {Identity.invalidate();});
+}
+}
