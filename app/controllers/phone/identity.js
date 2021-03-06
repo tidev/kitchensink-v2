c1870f88142c113f5d6b9c388810e9cce4d29245
Joshua Quick <jquick@axway.com>
2021-03-05 16:24:44 -0800

    refactor: ti.identity example to use passcode as fallback

--- app/controllers/phone/identity.js
+++ app/controllers/phone/identity.js
@@ -1,25 +1,25 @@
 import Identity from 'ti.identity';
 
-let authPhrase = '';
-
 function handleOpen() {
-	$.authenticate.title = 'Tap here to authenticate';
+	// Use passcode/pin-number as a fallback in case device doesn't support TouchID/FaceID.
+	Identity.authenticationPolicy = Identity.AUTHENTICATION_POLICY_PASSCODE;
 
+	// Do not continue if device does not support authentication.
 	if (!Identity.isSupported()) {
 		return;
 	}
 
+	// On iOS, we know ahead of time which authentication policy is available to us. Show it on button.
 	if (OS_IOS) {
+		let authPhrase = '';
 		if (Identity.biometryType == Identity.BIOMETRY_TYPE_FACE_ID) {
 			authPhrase = 'Face ID';
 		} else if (Identity.biometryType == Identity.BIOMETRY_TYPE_TOUCH_ID) {
 			authPhrase = 'Touch ID';
 		} else {
-			authPhrase = '(None available)';
+			authPhrase = 'Passcode';
 		}
 		$.authenticate.title = `Authenticate with: ${authPhrase}`;
-	} else if (OS_ANDROID) {
-		authPhrase = 'Fingerprint';
 	}
 }
 
@@ -29,37 +29,18 @@ function validate() {
 		return;
 	}
 
-	if (OS_ANDROID) {
-		$.androidFingerprint.setReason('Confirm fingerprint to authenticate');
-		$.androidFingerprint.setTitle('Identity');
-	}
-
 	Identity.authenticate({
 		reason: "Please authenticate to continue",
 		fallbackTitle: "",
 		callback: (e) => {
-			if (OS_IOS) {
-				Identity.invalidate();
-				if (!e.success) {
-					alert(e.error);
-				} else {
-					setTimeout(() => {
-						alert('Successfully authenticated!');
-					}, 1000);
-				}
-			} else if (OS_ANDROID) {
-				if (e.success) {
-					$.androidFingerprint.success();
-				} else {
-					$.androidFingerprint.failure(e.error);
-				}
+			Identity.invalidate();
+			if (!e.success) {
+				alert(e.error);
+			} else {
+				setTimeout(() => {
+					alert('Successfully authenticated!');
+				}, 1000);
 			}
 		}
 	});
-
-	if (OS_ANDROID) {
-		$.androidFingerprint.show(() => {
-			Identity.invalidate();
-		});
-	}
 }
