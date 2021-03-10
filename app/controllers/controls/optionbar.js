705f06c0ff24ea7259fbbbb5f3d21beb7f053c0c
Joshua Quick <jquick@axway.com>
2021-03-09 18:17:14 -0800

    feat: add Ti.UI.OptionBar example

---
+++ app/controllers/controls/optionbar.js
@@ -0,0 +1,19 @@
+import { logger } from 'logger';
+
+function onTextOptionBarClicked(e) {
+	const message = `Clicked on text-only button: ${e.source.labels[e.index].title}`;
+	alert(message);
+	logger.log(message);
+}
+
+function onImageOptionBarClicked({ index }) {
+	const message = `Clicked on image-only button index: ${index}`;
+	alert(message);
+	logger.log(message);
+}
+
+function onImageTextOptionBarClicked(e) {
+	const message = `Clicked on image/text button: ${e.source.labels[e.index].title}`;
+	alert(message);
+	logger.log(message);
+}
