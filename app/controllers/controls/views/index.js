02601519768cc174e47c3600c90a698c935c0303
Joshua Quick <jquick@axway.com>
2021-10-05 18:24:06 -0700

    feat: add ListView edit/selection example

--- app/controllers/controls/views/index.js
+++ app/controllers/controls/views/index.js
@@ -11,7 +11,9 @@ function openComponent(e) {
 	const identifier = 'controls/views/' + e.section.getItemAt(e.itemIndex).properties.itemId;
 	const component = Alloy.createController(identifier).getView();
 
-	Alloy.Globals.setAndroidBackButton(component);
+	if (OS_ANDROID && identifier !== 'controls/views/listviewMultiSelect') {
+		Alloy.Globals.setAndroidBackButton(component);
+	}
 	Alloy.CFG.tabGroup.activeTab.open(component);
 
 	logger.log('Ti.UI.TabGroup.activeTab.open', identifier);
