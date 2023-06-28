71c17bb2d7eccc3d1cbed27a83b339dad4181ff7
Hans Knöchel <h.knoechel@lambus.com>
2023-06-28 09:24:14 +0200

    fix: fix crash when opening the app for the first time

--- app/controllers/controls/index.js
+++ app/controllers/controls/index.js
@@ -7,6 +7,35 @@ import { logger } from 'logger';
 
 }());
 
+function onFocus() {
+	$.window.removeEventListener('focus', onFocus);
+	validateDocsInfo();
+}
+
+
+function validateDocsInfo() {
+	if (!Ti.App.Properties.getBool('noticeShown', false)) {
+		const alertNotice = Ti.UI.createAlertDialog({
+			title: 'Notice',
+			message: 'While this KitchenSink provides an extensive demonstration of the Titanium API, it does not include examples for every available component. Please refer to our documentation for more details.',
+			buttonNames: [ 'Alright!', 'Visit docs', 'Don\'t show again' ],
+			cancel: 0,
+			destructive: 2
+		});
+
+		alertNotice.addEventListener('click', ({ index }) => {
+			if (index === 1) {
+				Ti.Platform.openURL('https://titaniumsdk.com');
+			} else if (index === 2) {
+				Ti.App.Properties.setBool('noticeShown', true);
+			}
+		});
+
+		alertNotice.show();
+	}
+}
+
+
 function openComponent(e) {
 	const identifier = `controls/${e.section.getItemAt(e.itemIndex).properties.itemId}`;
 	const component = Alloy.createController(identifier).getView();
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,28 +12,6 @@ import { logger } from 'logger';
 	$.index.open();
 }());
 
-function validateDocsInfo() {
-	if (!Ti.App.Properties.getBool('noticeShown', false)) {
-		const alertNotice = Ti.UI.createAlertDialog({
-			title: 'Notice',
-			message: 'While this KitchenSink provides an extensive demonstration of the Titanium API, it does not include examples for every available component. Please refer to our documentation for more details.',
-			buttonNames: [ 'Alright!', 'Visit docs', 'Don\'t show again' ],
-			cancel: 0,
-			destructive: 2
-		});
-
-		alertNotice.addEventListener('click', event => {
-			if (event.index === 1) {
-				Ti.Platform.openURL('https://titaniumsdk.com');
-			} else if (event.index === 2) {
-				Ti.App.Properties.setBool('noticeShown', true);
-			}
-		});
-
-		alertNotice.show();
-	}
-}
-
 function handleShortcutItem(e) {
 	logger.log('Ti.App.iOS.shortcutitemclick', e);
 }
